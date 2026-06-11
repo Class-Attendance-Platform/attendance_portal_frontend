e5f1364bd2bf31796351cefda940538740c666e2
Grinch_ <user.grinch@gmail.com>
2026-06-11 21:47:17 +0600

    fix(ui): remove w-screen from admin semesters layout

--- app/dashboard/admin/semesters/_layout.tsx
+++ app/dashboard/admin/semesters/_layout.tsx
@@ -4,7 +4,7 @@ import { View } from "react-native";
 
 export default function RootLayout() {
     return (
-        <View className="w-screen h-screen">
+        <View className="flex-1">
             <Slot />
         </View>
     );
