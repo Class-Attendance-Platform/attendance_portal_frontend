7756b2a3b8b7ae70d77065d798d56541e136fd9c
Grinch_ <user.grinch@gmail.com>
2026-06-11 21:14:07 +0600

    cleanup

--- attendance_portal_backend/apps/academic/urls/teacher.py
+++ attendance_portal_backend/apps/academic/urls/teacher.py
@@ -1,7 +1,27 @@
 from django.urls import path
-from apps.academic.views.teacher import TeacherCoursesView, TeacherCourseInfoDetailView
+
+from apps.academic.views.teacher import (
+    TeacherCourseInfoDetailView,
+    TeacherCoursesView,
+    TeacherHistorySessionView,
+)
 
 urlpatterns = [
-    path('<uuid:uuid>/courses/', TeacherCoursesView.as_view(), name='teacher-courses'),
-    path('course-info/<uuid:uuid>/', TeacherCourseInfoDetailView.as_view(), name='teacher-course-info'),
+    path("<uuid:uuid>/courses/", TeacherCoursesView.as_view(), name="teacher-courses"),
+    path(
+        "course-info/<uuid:uuid>/",
+        TeacherCourseInfoDetailView.as_view(),
+        name="teacher-course-info",
+    ),
+    # Manual History Endpoints
+    path(
+        "course-info/<uuid:uuid>/history-session/",
+        TeacherHistorySessionView.as_view(),
+        name="teacher-history-session",
+    ),
+    path(
+        "course-info/<uuid:uuid>/history-session/<str:date>/",
+        TeacherHistorySessionView.as_view(),
+        name="teacher-history-session-detail",
+    ),
 ]
