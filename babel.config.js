24e9d837cf70e0fbb6fd2f82876475fbaa5e42b0
Grinch_ <user.grinch@gmail.com>
2026-06-29 22:44:28 +0600

    add files

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
     plugins: [
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-private-methods',
-      '@babel/plugin-transform-private-property-in-object',
-    ]
+      "@babel/plugin-transform-class-properties",
+      "@babel/plugin-transform-private-methods",
+      "@babel/plugin-transform-private-property-in-object",
+      "react-native-reanimated/plugin", // MUST BE LAST
+    ],
   };
 };
